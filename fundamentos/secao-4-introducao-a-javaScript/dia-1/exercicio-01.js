c780a8c731040c590e01d676ef7be784328d8249
Victor Pinheiro Mello <victormello7003@hotmail.com>
2022-09-21 15:15:39 -0300

    Iniciando seção 4 JavaScript

---
+++ fundamentos/secao-4-introducao-a-javaScript/dia-1/exercicio-01.js
@@ -0,0 +1,12 @@
+const myName = 'Victor Pinheiro Mello';
+const birthCity = 'Feira de Santana';
+let birthYear = '22/12/1997';
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = 'Salvador';
+console.log(birthCity);
