60f24590caba34b398f9053ad79181674fe2a404
Victor Pinheiro Mello <victormello7003@hotmail.com>
2022-09-22 15:46:05 -0300

    teinando loop for

---
+++ fundamentos/secao-4-introducao-a-javaScript/dia-2-array-e-loop-for/for.js
@@ -0,0 +1,14 @@
+let numero = 7;
+
+for (let i = 1; i < 10; i += 1) {
+  console.log(numero * i);
+}
+
+// i++ não recomendado | trocar por i += 1
+
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log():
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let i = 0; i < groceryList.length; i += 1) {
+  console.log(groceryList[i]);
+}
