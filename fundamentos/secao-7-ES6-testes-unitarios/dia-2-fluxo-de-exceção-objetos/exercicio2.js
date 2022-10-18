b3cc4b22e049a6d8e2a9ba5dfdbf64d1b2bc615d
Victor Pinheiro Mello <victormello7003@hotmail.com>
2022-10-18 20:38:57 -0300

    Exercício 2 do dia 7.2

---
+++ fundamentos/secao-7-ES6-testes-unitarios/dia-2-fluxo-de-exceção-objetos/exercicio2.js
@@ -0,0 +1,55 @@
+const order = {
+  name: "Rafael Andrade",
+  phoneNumber: "11-98763-1416",
+  address: {
+    street: "Rua das Flores",
+    number: "389",
+    apartment: "701",
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: "Coca-Cola Zero",
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: "Ana Silveira",
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  console.log(
+    `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, N°: ${order.address.number}, AP: ${order.address.apartment}.`
+  );
+};
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  order.order.delivery.deliveryPerson = "Luiz Silva";
+  order.order.pizza.marguerita.name = "marguerita";
+  order.order.pizza.pepperoni.name = "pepperoni";
+  order.payment.total = 50;
+  console.log(
+    `Olá ${order.order.delivery.deliveryPerson}, o total do seu pedido de ${order.order.pizza.marguerita.name}, ${order.order.pizza.pepperoni.name}, ${order.order.drinks.coke.type} é R$ ${order.payment.total},00`
+  );
+};
+
+orderModifier(order);
